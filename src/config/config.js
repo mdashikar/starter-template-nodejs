48774bcc54f220838a076641e126817f46e15793
Md. Ashiqur Rahman <md.ashik.ar@gmail.com>
2025-01-11 17:59:53 +0600

    refactor: simplify JWT configuration by removing expiration settings and consolidating secret keys

--- src/config/config.js
+++ src/config/config.js
@@ -11,14 +11,6 @@ const envVarsSchema = Joi.object()
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     JWT_ACCESS_TOKEN_SECRET: Joi.string().required().description('JWT access token secret key'),
     JWT_REFRESH_TOKEN_SECRET: Joi.string().required().description('JWT refresh token secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which reset password token expires'),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which verify email token expires'),
     SMTP_HOST: Joi.string().description('server that will send the emails'),
     SMTP_PORT: Joi.number().description('port to connect to the email server'),
     SMTP_USERNAME: Joi.string().description('username for email server'),
